3909117ff3997602f619d94dbaf00a8e99ecb45c
Michael Lin <linzichunzf@hotmail.com>
2016-09-29 17:57:59 -0600

    done

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -1,6 +1,9 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * Created by makepeac on 9/29/16.
@@ -16,12 +19,34 @@ public class TweetList {
         return tweets.get(index);
     }
 
+    // http://stackoverflow.com/questions/5207029/how-to-sort-a-list-of-objects-by-their-date-java-collections-listobject
+    public ArrayList<Tweet> getTweets() {
+        Collections.sort(tweets, new Comparator<Tweet>() {
+            public int compare(Tweet t1, Tweet t2) {
+                return t1.getDate().compareTo(t2.getDate());
+            }
+        });
+        return tweets;
+    }
+
+    public int getCount() {
+        return tweets.size();
+    }
     public boolean hasTweet(Tweet tweet){
+        for (Tweet t : tweets) {
+            if (t.equals(tweet)) {
+                return true;
+            }
+        }
         return tweets.contains(tweet);
     }
 
     public void add(Tweet tweet) {
-        tweets.add(tweet);
+        if (tweets.contains(tweet)) {
+            throw new IllegalArgumentException();
+        } else {
+            tweets.add(tweet);
+        }
     }
 
     public void delete(Tweet tweet) {
